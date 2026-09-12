e3d4355d09e1f286c067e9f9167fbc37fcabcb3e
shahinreza9702 <shahinr9701@hotmail.com>
2026-09-12 19:21:26 +0600

    fix github deployment problem

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'devstack-assignment',
+  base: "/devstack-assignment/",
 });
